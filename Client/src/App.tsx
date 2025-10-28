0c05b961d6a315ce475694a665453fbbf4e25a44
Dalar-Coin <Dalarcoinmusic@gmail.com>
2025-10-28 11:54:43 -0400

    updated folder structure

---
+++ Client/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/SignUp" element={<SignUp />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
